765c07dd5a5267e447327fbc654ff33a318cdd9b
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-06 23:46:01 +0500

    Que 9: Favorite Number

---
+++ Main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Que 9: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that
+// reveals your favorite number. Print that message.
+const favorite_Number = 10;
+const message = "Here is my favorite number:" + " " + favorite_Number;
+console.log(message);
---
+++ Main.ts
@@ -0,0 +1,6 @@
+// Que 9: Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that
+// reveals your favorite number. Print that message.
+
+const favorite_Number: number = 10;
+const message: string = "Here is my favorite number:" + " " + favorite_Number;
+console.log(message);
